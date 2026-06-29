5c050235df4e14b357d8568113d08c4a9932ae0c
Rukmagat Kandel <101416363+sunder55@users.noreply.github.com>
2026-06-29 12:29:39 +0545

    changes

--- functions.php
+++ functions.php
@@ -5,23 +5,6 @@ require_once get_template_directory() . '/inc/setup.php';
 require_once get_template_directory() . '/inc/custom-post-types.php';
 require_once get_template_directory() . '/inc/enqueue.php';
 require_once get_template_directory() . '/inc/contact.php';
-require_once get_template_directory() . '/inc/seo.php';
-
-function wpdev_custom_document_title($title)
-{
-    if (is_front_page()) {
-        $title['title'] = get_bloginfo('name');
-        unset($title['tagline']);
-    }
-    return $title;
-}
-add_filter('document_title_parts', 'wpdev_custom_document_title');
-
-function wpdev_document_title_separator($sep)
-{
-    return ' - ';
-}
-add_filter('document_title_separator', 'wpdev_document_title_separator');
 
 function wpdev_get_field($field_name, $post_id = null)
 {
